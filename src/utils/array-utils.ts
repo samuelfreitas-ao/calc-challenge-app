f4c12788f2342503f38acc934c12781ba6d08128
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-04-21 00:30:35 +0100

    feat: array utils

---
+++ src/utils/array-utils.ts
@@ -0,0 +1,39 @@
+type Params = {
+  array: any[]
+  fieldToOrder?: string
+  order: 'asc' | 'desc'
+}
+export class ArrayUtils {
+  static sort<T = any>({ array, fieldToOrder, order }: Params): T {
+    if (!fieldToOrder) {
+      return [...array].sort((a, b) => {
+        switch (order) {
+          case 'asc':
+            return a.localeCompare(b)
+            if (a > b) return 1
+            if (a < b) return -1
+            return 0
+          default:
+            if (a > b) return -1
+            if (a < b) return 1
+            return 0
+        }
+      }) as T
+    }
+
+    return [...array].sort((a, b) => {
+      switch (order) {
+        case 'asc':
+          return b[fieldToOrder].localeCompare(a[fieldToOrder])
+        // if (a[fieldToOrder] > b[fieldToOrder]) return 1
+        // if (a[fieldToOrder] < b[fieldToOrder]) return -1
+        // return 0
+        default:
+          return a[fieldToOrder].localeCompare(b[fieldToOrder])
+        // if (a[fieldToOrder] > b[fieldToOrder]) return -1
+        // if (a[fieldToOrder] < b[fieldToOrder]) return 1
+        // return 0
+      }
+    }) as T
+  }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './array-utils'
 export * from './date-utils'
 export * from './number-utils'
 export * from './question-utils'
