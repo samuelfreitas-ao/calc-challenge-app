4fef94292b73e5856a42e63f90104810a0630b07
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-04-20 21:54:17 +0100

    feat: add question type

---
+++ src/@types/question.ts
@@ -0,0 +1,18 @@
+export enum IOperator {
+  plus = '+',
+  multiply = 'x',
+  division = '/',
+  minus = '-',
+}
+export type IQuestion = {
+  value1: number
+  value2: number
+  operator: IOperator
+  answer: number
+
+  isRight: boolean
+  seconds: number
+  time: string
+
+  date: Date
+}
