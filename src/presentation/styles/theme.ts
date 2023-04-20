626575f14510f2d23a0ede8de4115bccbe2749bb
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-04-20 16:03:33 +0100

    style: add THEME

---
+++ src/presentation/styles/theme.ts
@@ -0,0 +1,31 @@
+export const THEME = {
+  colors: {
+    green: {
+      700: '#4A7D5B',
+      600: '#5B7865',
+    },
+    red: {
+      700: '#A05858',
+    },
+    gray: {
+      800: '#555050',
+      700: '#756A6A',
+      600: '#90908E',
+      500: '#ADADAB',
+      200: '#D9D9D9',
+    },
+  },
+  fonts: {
+    heading: 'Exo_700Bold',
+    medium: 'Exo_500Medium',
+    body: 'Exo_400Regular',
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 32,
+    '2xl': 48,
+  },
+}
