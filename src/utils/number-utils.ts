8c0baf1a411acdd895498d9b87e29b500a82704f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-04-22 20:45:23 +0100

    refactor: include min and max on random return

--- src/utils/number-utils.ts
+++ src/utils/number-utils.ts
@@ -1,5 +1,5 @@
 export class NumberUtils {
   public static random(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min) + min)
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
 }
