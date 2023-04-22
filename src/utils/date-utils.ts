33b06617b6971f407ffa7aff6619191348e8f13d
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-04-22 23:37:10 +0100

    feat: getDate

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -11,4 +11,17 @@ export class DateUtils {
 
     return `${h}:${i}:${s}`
   }
+
+  static getDate(date: Date) {
+    if (date instanceof Date) {
+      const day = date.getDate().toString().padStart(2, '0'),
+        month = (date.getMonth() + 1).toString().padStart(2, '0'),
+        year = date.getFullYear(),
+        h = date.getHours().toString().padStart(2, '0'),
+        i = date.getMinutes().toString().padStart(2, '0'),
+        s = date.getSeconds().toString().padStart(2, '0')
+      return `${day}/${month}/${year} ${h}:${i}:${s}`
+    }
+    return ''
+  }
 }
