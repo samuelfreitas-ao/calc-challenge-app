f78b11274826d0f52ed12baaac8dd6968c59be50
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-04-23 12:09:34 +0100

    refactor: separate button styles and divide in component

---
+++ src/presentation/components/button/index.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react"
+import {
+  Pressable,
+  PressableProps,
+  StyleSheet,
+} from "react-native"
+
+interface ButtonProps extends PressableProps {
+  children: ReactNode
+}
+
+export * from './simple-button'
+
+export function Button ({ children, style, ...props }: ButtonProps) {
+  return (
+    <Pressable
+      style={({ pressed }) => [Styles.constainer, { opacity: pressed ? 0.7 : 1 }, style as any]}
+      {...props}
+    >
+      {children}
+    </Pressable>
+  )
+}
+
+const Styles = StyleSheet.create({
+  constainer: {
+    alignItems: 'center',
+    gap: 8,
+    padding: 16,
+    borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+})
---
+++ src/presentation/components/button/simple-button/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+import { StyleSheet, TouchableOpacity, TouchableOpacityProps } from "react-native"
+
+interface SimpleButtonProps extends TouchableOpacityProps {
+  children: ReactNode
+}
+
+export function SimpleButton ({ children, style, ...props }: SimpleButtonProps) {
+  return (
+    <TouchableOpacity {...props}>
+      {children}
+    </TouchableOpacity>
+  )
+}
