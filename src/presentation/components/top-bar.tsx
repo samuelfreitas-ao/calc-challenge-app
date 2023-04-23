3745db5cc182498ebb13d7426ab627034911ca9e
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-04-23 12:36:48 +0100

    styles: separate top bar styles

--- src/presentation/components/top-bar.tsx
+++ src/presentation/components/top-bar.tsx
@@ -1,40 +1,38 @@
-import { View, StatusBar } from 'react-native'
+import { View, StatusBar, StyleSheet } from 'react-native'
 import { THEME } from '../styles/theme'
 import { DateUtils } from '../../utils'
 import { Points } from './points'
 import { Text } from './text'
 import { useApp } from '../../hooks'
 
-type Props = {
-  timer: number
-}
+type Props = { timer: number }
 
 export function TopBar ({ timer }: Props) {
   const { historyList } = useApp()
+  const wrongPoints = historyList.filter(quest => !quest.isRight).length
+  const rightPoints = historyList.filter(quest => quest.isRight).length
+
   return (
-    <View style={{
-      paddingTop: 24 + StatusBar.currentHeight || 0,
-      paddingHorizontal: 24,
-      paddingBottom: 24,
-      backgroundColor: THEME.colors.gray[200],
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center'
-    }}>
-      <Points
-        points={historyList.filter(quest => !quest.isRight).length}
-        type="wrong"
-      />
-      <Text text={DateUtils.secondsToTime(timer)}
-        style={{
-          fontFamily: THEME.fonts.heading,
-          fontSize: THEME.fontSizes.md
-        }}
-      />
-      <Points
-        points={historyList.filter(quest => quest.isRight).length}
-        type="right"
-      />
+    <View style={styles.container}>
+      <Points points={wrongPoints} type="wrong" />
+      <Text text={DateUtils.secondsToTime(timer)} style={styles.timer} />
+      <Points points={rightPoints} type="right" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 24 + StatusBar.currentHeight || 0,
+    paddingHorizontal: 24,
+    paddingBottom: 24,
+    backgroundColor: THEME.colors.gray[200],
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  timer: {
+    fontFamily: THEME.fonts.heading,
+    fontSize: THEME.fontSizes.md
+  }
+})
